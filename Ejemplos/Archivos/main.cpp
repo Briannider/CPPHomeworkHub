7ca022e3d8ba0753e8d5e318b92b18481d3190df
Brian Carballo <braianniderhaus@gmail.com>
2023-10-30 01:46:18 -0300

    Archivos:(fopen,fclose,tipos de aperturas y lectura)

--- Ejemplos/Archivos/main.cpp
+++ Ejemplos/Archivos/main.cpp
@@ -1,53 +1,42 @@
 #include <iostream>
-#include <cstdio>
-#include <stdio.h>
-#include <string.h>
 #include "funciones.hpp"
-using namespace std;
+using namespace std; //El usar el espacio de nombres std hace que evites tener que usar el std::cout o std::cin utilizando solo cout y cin
 
 int main()
 {
-     
+     /*
+     TIPOS DE APERTURA DE ARCHIVOS UTILIZANDO fopen() :
 
-    /* ----------------------------LECTURA DE ARCHIVOS------------------------------ 
-    FILE *arch;
-    arch = fopen("data/Alumno.dat", "rb+");
-    char nombre[21];
+     w   ==> Crea un fichero de escritura. Si ya existe lo crea de nuevo. 
+     w+  ==> Crea un fichero de lectura y escritura. Si ya existe lo crea de nuevo. 
+     a   ==> Abre o crea un fichero para añadir datos al final del mismo.
+     a+  ==> Abre o crea un fichero para leer y añadir datos al final del mismo.
+     r   ==> Abre un fichero de lectura.
+     r+  ==> Abre un fichero de lectura y escritura. 
+
+     */
+
+
+
+
+    /* ----------------------------LECTURA DE ARCHIVOS------------------------------ */
+
+    FILE *arch; //Asignamos un puntero del tipo FILE
+    arch = fopen("Alumno.dat", "rb+"); //A traves de la funcion fopen, ingresamos como parametro ("Ubicacion del archivo", "Tipo de apertura" )
+    char nombre[21]; //Declaramos el tipo de dato que estamos queriendo leer del archivo 
+                    //"El dato ya debe estar en el archivo almacenado con anterioridad para su lectura"
 
     // LEER EL ARCHIVO
     cout<<"Lista de nombres dentro de un archivo: "<<endl;
     cout<<""<<endl;
-    while (!feof(arch))
+    while (!feof(arch)) //feof() es una funcion propia de C al igual que fopen() y otras. Su principal funcion es indicar el final del archivo.
     {
-        fread(&nombre, sizeof(nombre), 1, arch);
+        fread(&nombre, sizeof(nombre), 1, arch); //fread() esta funcion se encarga de leer el archivo los parametros son los siguientes:
+        //fread(&TipodeDato, Tamañodeldato,cantidadDatos,PunteroalArchivo);
         cout << nombre << endl;
         cout << "------" << endl;
     }
-    */
+    fclose(arch); //Siempre tenemos que cerrar el archivo cuando terminamos de usarlo.
 
-
-/*  EJERCICIO 1 : 
-
-Se tienen dos archivos “deposito1.dat” y “deposito2.dat” con los siguientes datos: 
--nroArt(numérico) 
--descripción (50 caracteres)
--cantidad (numérico).
-Estos archivos se encuentran 
-ordenados por nroArt y los artículos no se repiten entre depósitos (cada depósito maneja sus 
-artículos). Generar un archivo “stock.dat”, consolidando los artículos de ambos depósitos.
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-   return 0;
+    return 0;
 }
