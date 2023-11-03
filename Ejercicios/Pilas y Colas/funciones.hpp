2233b4f8ff82629fdc7ad06b27038c2d617ba2a5
Brian Carballo <braianniderhaus@gmail.com>
2023-11-02 23:36:45 -0300

    Pilas y Colas release

--- Ejercicios/Pilas y Colas/funciones.hpp
+++ Ejercicios/Pilas y Colas/funciones.hpp
@@ -1,15 +1,13 @@
 #include "funciones.cpp"
-
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
 /*Aqui se definiran las funciones y estructuras a utilizar en el archivo main.cpp*/
-
 #ifndef funciones
 #define funciones
 
-
-
-void encolar(Nodo* frte, Nodo* fin, int v);
-void desencolar();
-
-
+void encolar(Nodo *&frte, Nodo *&fin, int v);
+int desencolar(Nodo *&frte, Nodo *&fin);
 
 #endif
