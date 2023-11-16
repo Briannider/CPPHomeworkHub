813d1ae7dafc8b4e6a4f75e33729d03f2ba3c922
Brian Carballo <84468272+Briannider@users.noreply.github.com>
2023-11-15 22:03:04 -0300

    Update main.cpp
    Main tp integrador final

--- Ejercicios/TP INTEGRADOR/main.cpp
+++ Ejercicios/TP INTEGRADOR/main.cpp
@@ -1,22 +1,16 @@
 #include "funciones.hpp"
 #include <iostream>
-#include <string>
 #include <string.h>
+#include <string>
 using namespace std;
 
-int main()
-{
-	NodoMenu *Menu = NULL;
-	FILE *F;
-	F = fopen("Precios.dat", "wb+");
-	if (F == NULL)
-	{
-		cout << "Error al abrir el archivo" << endl;
-	}
-	// NodoCliente*  cliente = NULL;
-	// NodoPedidos* Pedidos = NULL;
-	// NodoPedidosCola * PedidosCola = NULL;
-	OpcionesPrincipales(Menu, F);
-	fclose(F);
-	return 0;
+int main() {
+NodoMenu *Menu = NULL;
+NodoCliente *Cliente = NULL;
+
+FILE *F;
+  
+  // NodoPedidos* Pedidos = NULL;
+  OpcionesPrincipales(Menu, Cliente, F);
+  return 0;
 }
