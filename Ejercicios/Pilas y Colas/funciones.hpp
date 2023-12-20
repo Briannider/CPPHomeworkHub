1eac28ed67355374fb9690eceb693ab8a9569400
Brian Carballo <braianniderhaus@gmail.com>
2023-12-19 22:54:54 -0300

    add: Pilas y Colas

--- Ejercicios/Pilas y Colas/funciones.hpp
+++ Ejercicios/Pilas y Colas/funciones.hpp
@@ -1,11 +1,98 @@
-#include "funciones.cpp"
-#include <iostream>
+#include <string>
+#include <string.h>
 using namespace std;
-/*Aqui se definiran las funciones y estructuras a utilizar en el archivo main.cpp*/
+
+struct Nodo
+{
+	int info;
+	Nodo *sgte;
+};
+
+struct NodoChar
+{
+	char info;
+	NodoChar *sgte;
+};
+
+struct NodoString
+{
+	string info;
+	NodoString *sgte;
+};
+
+struct infoEstudiante
+{
+	int legajo;
+	string nya;
+	int curso;
+};
+
+struct NodoEstudiante
+{
+	infoEstudiante info;
+	NodoEstudiante *sgte;
+};
+
 #ifndef funciones
 #define funciones
 
-void encolar(Nodo *&frte, Nodo *&fin, int v);
-int desencolar(Nodo *&frte, Nodo *&fin);
-
+void agregar(int arr[], int n, int &len, int v);
+void mostrar(int arr[], int len);
+int buscar(int arr[], int len, int v);
+void eliminar(int arr[], int &len, int pos);
+void insertar(int arr[], int &len, int v, int pos);
+int insertarOrdenado(int arr[], int &len, int v);
+int buscaEInserta(int arr[], int &len, int v, bool &enc);
+void inicializar(int arr[], int n);
+void burbujaAsc(int arr[], int len);
+int busquedaBinaria(int arr[], int len, int v); // el vector tiene que estar ordenado
+// Listas
+void agregarNodo(Nodo *&lista, int x);
+void mostrar(Nodo *lista);
+void liberar(Nodo *&lista);
+Nodo *buscar(Nodo *lista, int v);
+void eliminar(Nodo *&lista, int v);
+int eliminarPrimerNodo(Nodo *&lista);
+Nodo *insertarOrdenado(Nodo *&lista, int v);
+void ordenar(Nodo *&lista);
+Nodo *buscaEInsertaOrdenado(Nodo *&lista, int v, bool &enc);
+// Pilas
+void push(Nodo *&pila, int v);
+int pop(Nodo *&pila);
+// Colas
+void encolar(Nodo *&colafte, Nodo *&colafin, int v);
+int desencolar(Nodo *&colafte, Nodo *&colafin);
+// Pilas- Ej 1
+void colocarPrimero(Nodo *&pila, int v);
+// Pilas- Ej 2
+char colocarTercero(Nodo *&pila, int v);
+// Pilas- Ej 3
+char colocarXenPosicionY(Nodo *&pila, int x, int posY);
+// Pilas- Ej 4
+void modificarValorXporValorY(Nodo *&pila, int x, int y);
+// Pilas- Ej 5
+void push(NodoChar *&pila, char v);
+char pop(NodoChar *&pila);
+void encolar(NodoChar *&colafte, NodoChar *&colafin, char v);
+char desencolar(NodoChar *&colafte, NodoChar *&colafin);
+bool esInversa(NodoChar *&pila, NodoChar *&colafte, NodoChar *&colafin);
+// Pilas- Ej 6
+void push(NodoString *&pila, string v);
+string pop(NodoString *&pila);
+// Pila- Ej 7
+void ordenarPila(Nodo *&pila);
+// Colas - Ej 1
+void encolar(NodoEstudiante *&colafte, NodoEstudiante *&colafin, infoEstudiante v);
+infoEstudiante desencolar(NodoEstudiante *&colafte, NodoEstudiante *&colafin);
+char eliminarDosNodos(NodoEstudiante *&colafte, NodoEstudiante *&colafin);
+// Colas - Ej 2
+int contarNodos(NodoEstudiante *&colafte, NodoEstudiante *&colafin);
+// Colas - Ej 3
+void unionColas(NodoEstudiante *&colafteA, NodoEstudiante *&colafinA,
+				NodoEstudiante *&colafteB, NodoEstudiante *&colafinB,
+				NodoEstudiante *&colafteC, NodoEstudiante *&colafinC);
+// Colas - Ej 4
+void push(NodoEstudiante *&pila, infoEstudiante v);
+infoEstudiante pop(NodoEstudiante *&pila);
+void imprimirCola(NodoEstudiante *&colafte, NodoEstudiante *&colafin);
 #endif
--- Ejercicios/Pilas y Colas/main.cpp
+++ Ejercicios/Pilas y Colas/main.cpp
@@ -4,19 +4,283 @@
 #include <string.h>
 using namespace std;
 
-int main(){
+int main()
+{
+	/*Nodo* pila = NULL;
+	push(pila,4);
+	push(pila,5);
+	push(pila,7);
 
-    Nodo *frte = NULL;
-    Nodo *fin = NULL;
-    encolar(frte, fin, 1);
-    encolar(frte, fin, 2);
-    encolar(frte, fin, 3);
-    encolar(frte, fin, 4);
-    encolar(frte, fin, 5);
+	while(pila != NULL){
+	   cout << pop(pila) << endl;
+	}
+	*/
 
-    while (fin != NULL)
-    {
-        cout << desencolar(frte, fin) << endl;
-    }
-    return 0;
+	/*Nodo* colafte = NULL;
+	Nodo* colafin = NULL;
+
+	encolar(colafte,colafin,4);
+	encolar(colafte,colafin,5);
+	encolar(colafte,colafin,7);
+
+	while(colafte != NULL){
+	   cout << desencolar(colafte, colafin) << endl;
+	}
+	*/
+
+	// Pilas - Ej 1
+	/*Nodo * pila = NULL;
+	push(pila,4);
+	push(pila,3);
+	push(pila,5);
+	push(pila,6);
+	push(pila,7);
+	colocarPrimero(pila,10);
+
+	while(pila != NULL){
+	  cout << pop(pila) << endl;
+	}
+	*/
+
+	// Pilas - Ej 2
+	/*	Nodo* pila = NULL;
+	  push(pila,4);
+	  push(pila,3);
+	  push(pila,5);
+	  push(pila,6);
+	  push(pila,7);
+
+	  char fueExitoso = colocarTercero(pila,10);
+
+	  cout << "El proceso fue existoso: " << fueExitoso << endl;
+
+	  while(pila != NULL){
+		  cout << pop(pila) << endl;
+	  }
+	  */
+
+	// Pilas - Ej 3
+	/*Nodo* pila = NULL;
+	push(pila,4);
+	push(pila,3);
+	push(pila,5);
+	push(pila,6);
+	push(pila,7);
+
+	char fueExitoso = colocarXenPosicionY(pila,10,5);
+	cout << "El proceso fue existoso: " << fueExitoso << endl;
+
+	while(pila != NULL){
+		cout << pop(pila) << endl;
+	}
+	*/
+	// Pilas - Ej 4
+	/*Nodo* pila = NULL;
+	push(pila,10);
+	push(pila,3);
+	push(pila,10);
+	push(pila,6);
+	push(pila,10);
+	push(pila,7);
+
+	modificarValorXporValorY(pila,30,20);
+
+	while(pila != NULL){
+		cout << pop(pila) << endl;
+	}
+	*/
+	// Pilas - Ej 5
+	/*
+	NodoChar* pila = NULL;
+	NodoChar* colafte = NULL;
+	NodoChar* colafin = NULL;
+	char letra;
+	bool segundaParte = false;
+
+	cout << "Ingresar por teclado una letra y apretar * para salir" << endl;
+	cin >> letra;
+
+	while(letra != '*'){
+
+		if(letra == '.'){
+			segundaParte = true;
+		} else {
+			if(segundaParte){
+				encolar(colafte,colafin,letra);
+			} else {
+				push(pila,letra);
+			}
+		}
+
+		cout << "Ingresar por teclado una letra y apretar * para salir" << endl;
+		cin >> letra;
+	}
+
+	if(esInversa(pila,colafte,colafin)){
+		cout << "Los conjuntos son inversos" << endl;
+	} else {
+		cout << "Los conjuntos no son inversos" << endl;
+	}
+
+	*/
+	// Pilas - Ej 6
+	/*
+
+	NodoString* pila = NULL;
+	string apellido;
+	cout << "Ingresar un apellido, presione * para salir" << endl;
+	cin >> apellido;
+
+	while (apellido != "*"){
+		push(pila,apellido);
+
+		cout << "Ingresar un apellido, presione * para salir" << endl;
+		cin >> apellido;
+	}
+
+	while(pila != NULL){
+		cout << pop(pila) << endl;
+	}
+	*/
+
+	// Pilas - Ej 7
+	/*Nodo* pila = NULL;
+	push(pila,20);
+	push(pila,3);
+	push(pila,9);
+	push(pila,6);
+	push(pila,35);
+	push(pila,12);
+
+	ordenarPila(pila);
+
+	while(pila != NULL){
+		cout << pop(pila) << endl;
+	}
+  */
+
+	// Colas - Ejercicio 1
+	/*NodoEstudiante* colafte = NULL;
+	NodoEstudiante* colafin = NULL;
+
+	infoEstudiante est;
+
+	est.legajo = 24423;
+	est.nya = "perez";
+	est.curso = 1054;
+
+	encolar(colafte,colafin,est);
+
+	est.legajo = 46473;
+	est.nya = "lopez";
+	est.curso = 1053;
+
+	encolar(colafte,colafin,est);
+
+	char fueExitoso = eliminarDosNodos(colafte,colafin);
+	cout << "El proceso fue existoso: " << fueExitoso << endl;
+	*/
+	// Colas - Ejercicio 2
+	/*NodoEstudiante* colafte = NULL;
+	NodoEstudiante* colafin = NULL;
+
+	infoEstudiante est;
+
+	est.legajo = 24423;
+	est.nya = "perez";
+	est.curso = 1054;
+
+	encolar(colafte,colafin,est);
+
+	est.legajo = 46473;
+	est.nya = "lopez";
+	est.curso = 1053;
+
+	encolar(colafte,colafin,est);
+
+	int cantNodos = contarNodos(colafte,colafin);
+	cout << "La cola tiene: " << cantNodos << " nodos " << endl;
+	*/
+	// Colas - Ejercicio 3
+	/*
+	NodoEstudiante* colafteA = NULL;
+	NodoEstudiante* colafinA = NULL;
+
+	infoEstudiante est;
+
+	est.legajo = 24423;
+	est.nya = "perez";
+	est.curso = 1054;
+
+	encolar(colafteA,colafinA,est);
+
+	est.legajo = 46473;
+	est.nya = "lopez";
+	est.curso = 1053;
+
+	encolar(colafteA,colafinA,est);
+
+	NodoEstudiante* colafteB = NULL;
+	NodoEstudiante* colafinB = NULL;
+
+	est.legajo = 23523;
+	est.nya = "diaz";
+	est.curso = 1054;
+
+	encolar(colafteB,colafinB,est);
+
+	est.legajo = 34473;
+	est.nya = "dieguez";
+	est.curso = 1053;
+
+	encolar(colafteB,colafinB,est);
+
+	NodoEstudiante* colafteC = NULL;
+	NodoEstudiante* colafinC = NULL;
+
+	unionColas(colafteA,colafinA,colafteB,colafinB,colafteC,colafinC);
+
+	while(colafteC != NULL){
+	  est = desencolar(colafteC,colafinC);
+	  cout << est.nya << endl;
+	}
+	cout << "Cola A" << endl;
+	while(colafteA != NULL){
+	  est = desencolar(colafteA,colafinA);
+	  cout << est.nya << endl;
+	}
+
+	*/
+	// Colas - Ejercicio 4
+	/*
+	NodoEstudiante* colafte = NULL;
+	NodoEstudiante* colafin = NULL;
+
+   infoEstudiante est;
+
+	est.legajo = 24423;
+	est.nya = "perez";
+	est.curso = 1054;
+
+	encolar(colafte,colafin,est);
+
+	est.legajo = 46473;
+	est.nya = "lopez";
+	est.curso = 1053;
+
+	encolar(colafte,colafin,est);
+
+	est.legajo = 23523;
+	est.nya = "diaz";
+	est.curso = 1054;
+
+	encolar(colafte,colafin,est);
+
+	est.legajo = 34473;
+	est.nya = "dieguez";
+	est.curso = 1053;
+
+	encolar(colafte,colafin,est);
+	imprimirCola(colafte,colafin);
+	*/
 }
