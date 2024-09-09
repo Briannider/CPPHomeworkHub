d6a999e03e29c671556a9b51920d5bd81a963582
Brian Carballo <braianniderhaus@gmail.com>
2024-09-08 22:06:06 -0300

    fix: :bug: File Loop
    The file arch stayed in infinite loop because the "while" sentence
    
    While

--- Teoria y Ejemplos/Archivos/main.cpp
+++ Teoria y Ejemplos/Archivos/main.cpp
@@ -4,39 +4,35 @@ using namespace std; /* El usar el espacio de nombres std hace que evites tener 
 
 int main()
 {
-     /*
-     TIPOS DE APERTURA DE ARCHIVOS UTILIZANDO fopen() :
+    /*
+    TIPOS DE APERTURA DE ARCHIVOS UTILIZANDO fopen() :
 
-     w   ==> Crea un fichero de escritura. Si ya existe lo crea de nuevo. 
-     w+  ==> Crea un fichero de lectura y escritura. Si ya existe lo crea de nuevo. 
-     a   ==> Abre o crea un fichero para añadir datos al final del mismo.
-     a+  ==> Abre o crea un fichero para leer y añadir datos al final del mismo.
-     r   ==> Abre un fichero de lectura.
-     r+  ==> Abre un fichero de lectura y escritura. 
+    w   ==> Crea un fichero de escritura. Si ya existe lo crea de nuevo.
+    w+  ==> Crea un fichero de lectura y escritura. Si ya existe lo crea de nuevo.
+    a   ==> Abre o crea un fichero para añadir datos al final del mismo.
+    a+  ==> Abre o crea un fichero para leer y añadir datos al final del mismo.
+    r   ==> Abre un fichero de lectura.
+    r+  ==> Abre un fichero de lectura y escritura.
 
-     */
-
-
-
+    */
 
     /* ----------------------------LECTURA DE ARCHIVOS------------------------------ */
 
-    FILE *arch; //Asignamos un puntero del tipo FILE
-    arch = fopen("Alumno.dat", "rb+"); //A traves de la funcion fopen, ingresamos como parametro ("Ubicacion del archivo", "Tipo de apertura" )
-    char nombre[21]; //Declaramos el tipo de dato que estamos queriendo leer del archivo 
-                    //"El dato ya debe estar en el archivo almacenado con anterioridad para su lectura"
+    FILE *arch;                        // Asignamos un puntero del tipo FILE
+    arch = fopen("Alumno.dat", "rb+"); // A traves de la funcion fopen, ingresamos como parametro ("Ubicacion del archivo", "Tipo de apertura" )
+    char nombre[21];                   // Declaramos el tipo de dato que estamos queriendo leer del archivo
+                     //"El dato ya debe estar en el archivo almacenado con anterioridad para su lectura"
 
     // LEER EL ARCHIVO
-    cout<<"Lista de nombres dentro de un archivo: "<<endl;
-    cout<<""<<endl;
-    while (!feof(arch)) //feof() es una funcion propia de C al igual que fopen() y otras. Su principal funcion es indicar el final del archivo.
-    {
-        fread(&nombre, sizeof(nombre), 1, arch); //fread() esta funcion se encarga de leer el archivo los parametros son los siguientes:
-        //fread(&TipodeDato, Tamañodeldato,cantidadDatos,PunteroalArchivo);
-        cout << nombre << endl;
-        cout << "------" << endl;
-    }
-    fclose(arch); //Siempre tenemos que cerrar el archivo cuando terminamos de usarlo.
+    cout << "Lista de nombres dentro de un archivo: " << endl;
+    cout << "" << endl;
+
+    fread(&nombre, sizeof(nombre), 1, arch); // fread() esta funcion se encarga de leer el archivo los parametros son los siguientes:
+    // fread(&TipodeDato, Tamañodeldato,cantidadDatos,PunteroalArchivo);
+    cout << nombre << endl;
+    cout << "------" << endl;
+
+    fclose(arch); // Siempre tenemos que cerrar el archivo cuando terminamos de usarlo.
 
     return 0;
 }
