f5c437f749edc5090a22aa132d52ccdbd7134ea8
Brian Carballo <braianniderhaus@gmail.com>
2025-07-08 09:58:42 -0300

    fix: :bug: Really short bug, nothing important

--- Ejercicios/Clase Nro 1/main.cpp
+++ Ejercicios/Clase Nro 1/main.cpp
@@ -9,26 +9,26 @@ int main()
     int b;
     int c;
 
-    // cout << "Ingrese los valores: "<<endl;
-    // cout << "a = "<<endl;
-    // cin>>a;
-    // cout << "b = "<<endl;
-    // cin>>b;
+    cout << "Ingrese los valores: " << endl;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
 
-    // cout << "Los valores son: "
-    //      << "a = " << a << " b = " << b << endl;
-    // cout << "La suma de a y b es: " << sumar(a, b) << endl;
-    // cout << "La resta de a y b es: " << restar(a, b) << endl;
-    // cout << "La multiplicacion de a y b es: " << multiplicar(a, b) << endl;
+    cout << "Los valores son: "
+         << "a = " << a << " b = " << b << endl;
+    cout << "La suma de a y b es: " << sumar(a, b) << endl;
+    cout << "La resta de a y b es: " << restar(a, b) << endl;
+    cout << "La multiplicacion de a y b es: " << multiplicar(a, b) << endl;
     //! Ejercicio 2:
-    // cout << "Ingrese la primera nota: " << endl;
-    // cin >> a;
-    // cout << "Ingrese la segunda nota: " << endl;
-    // cin >> b;
-    // cout << "Ingrese la tercera nota: " << endl;
-    // cin >> c;
+    cout << "Ingrese la primera nota: " << endl;
+    cin >> a;
+    cout << "Ingrese la segunda nota: " << endl;
+    cin >> b;
+    cout << "Ingrese la tercera nota: " << endl;
+    cin >> c;
 
-    // cout << "El promedio es: " << (a + b + c) / 3 << endl;
+    cout << "El promedio es: " << (a + b + c) / 3 << endl;
 
     return 0;
 }
